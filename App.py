a374d7c81a9b4b699685bead90ffad42dc8f8e62
avadhootjadhav1290-gif <avadhootjadhav1290@gmail.com>
2026-05-27 15:11:28 +0530

    Fixed Groq model

--- App.py
+++ App.py
@@ -28,7 +28,7 @@ groq_api_key = st.secrets["GROQ_API_KEY"]
 # --------------------------------
 llm = ChatGroq(
     groq_api_key=groq_api_key,
-    model_name="llama3-70b-8192"
+    model_name="llama-3.1-8b-instant"
 )
 
 # --------------------------------
